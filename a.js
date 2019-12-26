e6debbae3cdbc6c94354f60009f91c40c0a302b3
liuweibo <wb-lwb414922@antfin.com>
2019-12-26 15:38:22 +0800

    deploy

---
+++ a.js
@@ -0,0 +1,12 @@
+console.log(100);
+console.log(200);
+alert(123);
+alert(400);
+alert(300);
+alert(600);
+function fn(){
+    var arr=[1,2,3,4];
+    var arr2=[3,4,5,6,7,8,9];
+    var arr3=[...arr,arr2]
+    return arr3;
+}
